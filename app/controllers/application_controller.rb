741526b7765946b508118c1f27a8988a47217981
Tami <catquarks@gmail.com>
2016-08-14 00:52:32 -0400

    Made a rudimentary blogging app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts' do 
+  	@posts = Post.all
+  	erb :posts    
   end
+
+  get '/posts/new' do 
+  	erb :new
+	end
+
+	post '/posts' do 
+		post_name = params[:name]
+		post_content = params[:content]
+		Post.create(name: post_name, content: post_content)
+		erb :posts
+	end
+
+	get '/posts/:id' do 
+		@post = Post.find(params[:id])
+		erb :show
+	end
+
+	get '/posts/:id/edit' do 
+		@post = Post.find(params[:id])
+		erb :edit
+	end
+
+	patch '/posts/:id' do 
+		post_name = params[:name]
+		post_content = params[:content]
+		post = Post.find(params[:id])
+		post.update(name: params[:name], content: params[:content])
+		@post = post
+		erb :show
+	end
+
+	delete '/posts/:id/delete' do 
+		@deleted_post = Post.find(params[:id])
+		erb :delete_confirm
+		
+	end
+
+
+
 end
